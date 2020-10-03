18832ae655cc3e7811eb6724cd5c9da90800a234
Kuss <2360314753@qq.com>
2020-10-03 20:59:13 +0800

    :children_crossing: improve: FilterHelper -> toPinyinWithAbbr

--- app/src/main/java/com/kuss/krude/utils/FilterHelper.kt
+++ app/src/main/java/com/kuss/krude/utils/FilterHelper.kt
@@ -4,21 +4,25 @@ import com.github.promeg.pinyinhelper.Pinyin
 
 class FilterHelper {
     companion object {
-        private fun toAbbr(full: String): String {
-            val isChinese = Pinyin.toPinyin(full, "") != full
-            if (isChinese) {
-                val pinyin = Pinyin.toPinyin(full, ",")
-                return pinyin.split(",").joinToString("") {
+        private fun toPinyinWithAbbr(label: String): String {
+            val pinyin = Pinyin.toPinyin(label, "")
+            val isChinese = pinyin != label
+            val abbr = if (isChinese) {
+                Pinyin.toPinyin(label, ",")
+                    .split(",")
+                    .joinToString("") {
+                        it.substring(0, 1)
+                    }
+            } else {
+                label.split(" ").joinToString("") {
                     it.substring(0, 1)
                 }
             }
-            return full.split(" ").joinToString("") {
-                it.substring(0, 1)
-            }
+            return "$pinyin, $abbr"
         }
 
         fun toTarget(label: String, packageName: String): String {
-            return "$label, $packageName, ${toAbbr(label)}"
+            return "$label, $packageName, ${toPinyinWithAbbr(label)}"
         }
     }
 }
