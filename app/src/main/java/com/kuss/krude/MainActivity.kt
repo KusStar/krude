0058d69aa8b3ea3a64999fdc4e1240e6b9b38a5a
Kuss <2360314753@qq.com>
2021-02-25 22:18:55 +0800

    :bug: fix: compat for Android 11

--- app/src/main/java/com/kuss/krude/MainActivity.kt
+++ app/src/main/java/com/kuss/krude/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.kuss.krude
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.kuss.krude.ui.AppListFragment
 import com.kuss.krude.ui.FilteredListFragment
 import com.kuss.krude.ui.MainFragment
 import com.kuss.krude.utils.PinyinHelper
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,14 +15,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .replace(R.id.list_container, AppListFragment.newInstance())
-                    .replace(R.id.filtered_list_container, FilteredListFragment.newInstance())
+                .replace(R.id.container, MainFragment.newInstance())
+                .replace(R.id.list_container, AppListFragment.newInstance())
+                .replace(R.id.filtered_list_container, FilteredListFragment.newInstance())
                 .commitNow()
         }
 
         PinyinHelper.initDict()
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+        }
     }
 
-    override fun onBackPressed() { }
+    override fun onBackPressed() {}
 }
