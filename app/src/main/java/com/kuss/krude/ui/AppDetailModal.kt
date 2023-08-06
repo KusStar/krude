0e74f53664c5990f9780b8bc072b3cb1642330e8
kusstar <kussssss@outlook.com>
2023-08-06 17:57:56 +0800

    refactor: ui composable

---
+++ app/src/main/java/com/kuss/krude/ui/AppDetailModal.kt
@@ -0,0 +1,220 @@
+package com.kuss.krude.ui
+
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetValue
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kuss.krude.R
+import com.kuss.krude.data.AppInfoWithIcon
+import com.kuss.krude.utils.ActivityHelper
+import kotlinx.coroutines.launch
+
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
+@Composable
+fun AppDetailModal(showAppDetailSheet: Boolean, selectedDetailApp: AppInfoWithIcon?, onClose: () -> Unit = {}) {
+    val context = LocalContext.current
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+
+    fun openAppInfo(item: AppInfoWithIcon) {
+        ActivityHelper.toDetail(context, item.packageName)
+    }
+
+    fun uninstallApp(item: AppInfoWithIcon) {
+        ActivityHelper.toUninstall(context, item.packageName)
+    }
+
+    if (showAppDetailSheet) {
+        ModalBottomSheet(
+            onDismissRequest = {
+                onClose()
+            },
+            sheetState = sheetState,
+            dragHandle = {
+                AnimatedContent(
+                    targetState = sheetState.currentValue,
+                    label = "DragHandle",
+                    transitionSpec = {
+                        scaleIn(
+                            initialScale = 0.92f,
+                            animationSpec = tween(220, delayMillis = 90)
+                        ).togetherWith(
+                            fadeOut(animationSpec = tween(90))
+                        )
+                    }) {
+                    when (it) {
+                        SheetValue.Expanded -> BottomSheetDefaults.DragHandle(
+                            modifier = Modifier.padding(
+                                vertical = 20.dp
+                            )
+                        )
+
+                        else -> BottomSheetDefaults.DragHandle()
+                    }
+                }
+            }
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                selectedDetailApp?.let { app ->
+                    val info = context.packageManager.getPackageInfo(
+                        app.packageName,
+                        PackageManager.GET_ACTIVITIES
+                    )
+
+                    AppItem(
+                        item = app,
+                        iconSize = 64.dp,
+                        titleFontSize = 20.sp, subtitleFontSize = 16.sp,
+                        enabled = false
+                    )
+                    // extra info
+                    Row {
+                        Text(
+                            text = info.versionName,
+                            textAlign = TextAlign.Center,
+                            color = MaterialTheme.colorScheme.secondary,
+                            fontSize = 14.sp,
+                        )
+                        val versionCode =
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) info.longVersionCode.toString() else info.versionCode.toString()
+                        Text(
+                            text = "($versionCode)",
+                            textAlign = TextAlign.Center,
+                            color = MaterialTheme.colorScheme.tertiary,
+                            fontSize = 14.sp,
+                        )
+                    }
+                    Spacing(3)
+                    // btns
+                    Row {
+                        Button(onClick = {
+                            openAppInfo(app)
+                        }) {
+                            val text = stringResource(id = R.string.app_info)
+                            Icon(
+                                Icons.Filled.Info,
+                                contentDescription = text,
+                                modifier = Modifier.size(ButtonDefaults.IconSize)
+                            )
+                            Spacing(1)
+                            Text(text = text)
+                        }
+                        Spacing(2)
+                        Button(onClick = {
+                            uninstallApp(app)
+                        }) {
+                            val text = stringResource(id = R.string.uninstall_app)
+                            Icon(
+                                Icons.Filled.Delete,
+                                contentDescription = text,
+                                modifier = Modifier.size(ButtonDefaults.IconSize)
+                            )
+                            Spacing(1)
+                            Text(text = text)
+                        }
+                    }
+
+                    Spacing(3)
+                    val activitiesListState = rememberLazyListState()
+                    if (info.activities.isNotEmpty()) {
+                        LazyColumn(state = activitiesListState) {
+                            stickyHeader {
+                                Column(
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .background(
+                                            MaterialTheme.colorScheme.background,
+                                            RoundedCornerShape(16.dp)
+                                        )
+                                ) {
+                                    TextButton(onClick = {
+                                        scope.launch {
+                                            activitiesListState.animateScrollToItem(0)
+                                        }
+                                    }) {
+                                        Text(
+                                            text = stringResource(id = R.string.all_activities),
+                                            fontSize = 16.sp,
+                                        )
+                                    }
+                                }
+                            }
+                            items(info.activities.filter { it.exported && it.enabled }) {
+                                TextButton(onClick = {  }) {
+                                    Column {
+                                        Text(
+                                            text = it.name,
+                                            color = MaterialTheme.colorScheme.primary,
+                                            fontSize = 16.sp,
+                                        )
+                                        Text(
+                                            text = it.packageName,
+                                            color = MaterialTheme.colorScheme.secondary,
+                                            fontSize = 12.sp,
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+//                    Button(onClick = {
+//                        scope.launch { sheetState.hide() }.invokeOnCompletion {
+//                            if (!sheetState.isVisible) {
+//                                showAppDetailSheet = false
+//                            }
+//                        }
+//                    }) {
+//                        Text("Close")
+//                    }
+            }
+
+        }
+    }
+}
---
+++ app/src/main/java/com/kuss/krude/ui/BottomSearchBar.kt
@@ -0,0 +1,181 @@
+package com.kuss.krude.ui
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kuss.krude.R
+import com.kuss.krude.data.AppInfoWithIcon
+import kotlinx.coroutines.launch
+import me.xdrop.fuzzywuzzy.FuzzySearch
+
+
+@Composable
+fun BottomSearchBar(
+    filtering: String,
+    setFiltering: (String) -> Unit,
+    items: List<AppInfoWithIcon>,
+    filteredItems: List<AppInfoWithIcon>,
+    setFilteredItems: (List<AppInfoWithIcon>) -> Unit,
+    openApp: (String) -> Unit,
+    toAppDetail: (AppInfoWithIcon) -> Unit
+) {
+    val scope = rememberCoroutineScope()
+    val focusRequester = remember {
+        FocusRequester()
+    }
+    AnimatedVisibility(
+        visible = filtering.isNotEmpty(),
+    ) {
+        Divider()
+        if (filteredItems.isNotEmpty()) {
+            LazyRow(
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(vertical = 8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                itemsIndexed(filteredItems) { index, item ->
+                    AppItem(
+                        modifier = Modifier.width(96.dp),
+                        item = item,
+                        titleFontSize = 14.sp,
+                        titleSingleLine = true,
+                        showSubtitle = false,
+                        onClick = {
+                            openApp(item.packageName)
+                        },
+                        onLongClick = {
+                            toAppDetail(item)
+                        }
+                    )
+                }
+            }
+        } else {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painter = painterResource(R.mipmap.ic_launcher_foreground),
+                    contentDescription = null,
+                    modifier = Modifier.size(96.dp)
+                )
+                Text(
+                    text = stringResource(id = R.string.no_match_app),
+                    color = MaterialTheme.colorScheme.secondary
+                )
+            }
+        }
+    }
+
+
+    Divider()
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        AnimatedVisibility(visible = filtering.isNotEmpty()) {
+            IconButton(onClick = { setFiltering("") }) {
+                Icon(
+                    Icons.Filled.Clear,
+                    contentDescription = "Clear",
+                    modifier = Modifier.size(ButtonDefaults.IconSize)
+                )
+            }
+        }
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .focusRequester(focusRequester),
+            value = filtering,
+            singleLine = true,
+            colors = TextFieldDefaults.colors(
+                unfocusedTextColor = MaterialTheme.colorScheme.secondary,
+                focusedTextColor = MaterialTheme.colorScheme.primary,
+                cursorColor = MaterialTheme.colorScheme.primary,
+                disabledContainerColor = Color.Transparent,
+                errorContainerColor = Color.Transparent,
+                focusedContainerColor = Color.Transparent,
+                unfocusedContainerColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent,
+                unfocusedPlaceholderColor = MaterialTheme.colorScheme.secondary,
+                focusedPlaceholderColor = MaterialTheme.colorScheme.primary
+            ),
+            onValueChange = { text ->
+                setFiltering(text)
+                scope.launch {
+                    val next = if (items.isNotEmpty())
+                    // TODO: options for fuzzy search and exact search
+//                            items.filter {
+//                                it.filterTarget.contains(
+//                                    text,
+//                                    ignoreCase = true
+//                                )
+//                            }
+                        items
+                            .map {
+                                val ratio = FuzzySearch.partialRatio(
+                                    it.abbr.lowercase(),
+                                    text.lowercase()
+                                ) + FuzzySearch.partialRatio(
+                                    it.filterTarget.lowercase(),
+                                    text.lowercase()
+                                )
+                                Pair(
+                                    it,
+                                    ratio
+                                )
+                            }
+                            .filter {
+                                it.second > 80
+                            }
+                            .sortedByDescending { it.second }
+                            .map {
+                                it.first
+                            }
+                    else emptyList()
+
+                    setFilteredItems(next)
+                }
+            },
+            placeholder = { Text(text = stringResource(id = R.string.search_placeholder)) },
+        )
+    }
+}
+
