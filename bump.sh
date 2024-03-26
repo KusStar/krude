c89ac2fc68c58c8f4ad723db84fe914a03cb4b81
Kusstar <kussssss@outlook.com>
2024-03-26 17:54:48 +0800

    feat: bump version in ci weekly_release

---
+++ bump.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+BUILD_GRADLE_PATH="./app/build.gradle"
+
+LATEST_COMMIT_HASH=$(git rev-parse --short HEAD)
+
+TIME_STAMP=$(date +%Y%m%d%H)
+
+sed -i '' -E "s/versionName \"[0-9.]+\"/versionName \"$TIME_STAMP.${LATEST_COMMIT_HASH}\"/" $BUILD_GRADLE_PATH
+
+echo "Updated versionName to $1 in $BUILD_GRADLE_PATH"
