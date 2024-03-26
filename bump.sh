d4e373fefee6e81fbd2146a2bdaf159bd1420def
kusstar <kussssss@outlook.com>
2024-03-26 20:58:53 +0800

    fix: bump.sh on ubuntu

--- bump.sh
+++ bump.sh
@@ -7,6 +7,6 @@ LATEST_COMMIT_HASH=$(git rev-parse --short HEAD)
 
 TIME_STAMP=$(date +%Y%m%d%H)
 
-sed -i '' -E "s/versionName \"[0-9.]+\"/versionName \"$TIME_STAMP.${LATEST_COMMIT_HASH}\"/" $BUILD_GRADLE_PATH
+sed -i"" -E "s/versionName \"[0-9.]+\"/versionName \"$TIME_STAMP.${LATEST_COMMIT_HASH}\"/" $BUILD_GRADLE_PATH
 
 echo "Updated versionName to $1 in $BUILD_GRADLE_PATH"
