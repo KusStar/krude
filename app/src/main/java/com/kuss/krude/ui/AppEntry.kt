c0ec9827515cbd7970d6fc568536945d2a4a15f6
Kusstar <kussssss@outlook.com>
2024-04-29 19:05:22 +0800

    feat: missingPremission check and AlertDialog

--- app/src/main/java/com/kuss/krude/ui/AppEntry.kt
+++ app/src/main/java/com/kuss/krude/ui/AppEntry.kt
@@ -7,6 +7,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.rememberLazyStaggeredGridState
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -16,14 +19,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.kuss.krude.R
 import com.kuss.krude.db.AppInfo
 import com.kuss.krude.ui.components.AppItem
 import com.kuss.krude.ui.components.AppItemShimmer
 import com.kuss.krude.utils.ActivityHelper
 import com.kuss.krude.viewmodel.MainViewModel
-import com.kuss.krude.viewmodel.SettingsViewModel
+import com.kuss.krude.viewmodel.settings.SettingsViewModel
 
 @Composable
 fun AppEntry(
@@ -36,6 +41,7 @@ fun AppEntry(
     val focusManager = LocalFocusManager.current
 
     val uiState by mainViewModel.state.collectAsState()
+    val missingPermission = uiState.missingPermission
     val apps = uiState.apps
     val scrollbarItems = uiState.scrollbarItems
 
@@ -63,58 +69,90 @@ fun AppEntry(
 
         focusManager.clearFocus()
     }
+    
+    if (!missingPermission) {
+        Column {
+            Row(modifier = Modifier.weight(1f, false)) {
+                LaunchedEffect(firstVisibleItemIndex) {
+                    val next = scrollbarItems.indexOfFirst {
+                        it == apps[firstVisibleItemIndex + 1].abbr.first().uppercase()
+                    }
 
-    Column {
-        Row(modifier = Modifier.weight(1f, false)) {
-            LaunchedEffect(firstVisibleItemIndex) {
-                val next = scrollbarItems.indexOfFirst {
-                    it == apps[firstVisibleItemIndex + 1].abbr.first().uppercase()
+                    mainViewModel.setSelectedHeaderIndex(next)
                 }
 
-                mainViewModel.setSelectedHeaderIndex(next)
-            }
+                LazyVerticalStaggeredGrid(
+                    state = listState,
+                    columns = StaggeredGridCells.Adaptive(128.dp),
+                    // content padding
+                    modifier = Modifier.weight(1f),
+                    contentPadding = PaddingValues(
+                        start = 12.dp,
+                        top = 16.dp,
+                        end = 12.dp,
+                        bottom = 12.dp
+                    ),
+                    content = {
+                        if (apps.isNotEmpty()) {
+                            items(apps.size) { index ->
+                                val item = apps[index]
 
-            LazyVerticalStaggeredGrid(
-                state = listState,
-                columns = StaggeredGridCells.Adaptive(128.dp),
-                // content padding
-                modifier = Modifier.weight(1f),
-                contentPadding = PaddingValues(
-                    start = 12.dp,
-                    top = 16.dp,
-                    end = 12.dp,
-                    bottom = 12.dp
-                ),
-                content = {
-                    if (apps.isNotEmpty()) {
-                        items(apps.size) { index ->
-                            val item = apps[index]
-
-                            AppItem(item = item,
-                                onClick = {
-                                    openApp(item)
-                                }, onLongClick = {
-                                    toAppDetail(item)
-                                })
-                        }
-                    } else {
-                        items(16) {
-                            AppItemShimmer()
+                                AppItem(item = item,
+                                    onClick = {
+                                        openApp(item)
+                                    }, onLongClick = {
+                                        toAppDetail(item)
+                                    })
+                            }
+                        } else {
+                            items(16) {
+                                AppItemShimmer()
+                            }
                         }
                     }
-                }
-            )
-            AlphabetScrollbar(mainViewModel = mainViewModel, listState = listState)
+                )
+                AlphabetScrollbar(mainViewModel = mainViewModel, listState = listState)
+            }
+            BottomSearchBar(
+                mainViewModel,
+                settingsViewModel,
+                openApp = {
+                    openApp(it)
+                },
+                toAppDetail = {
+                    toAppDetail(it)
+                })
         }
-        BottomSearchBar(
-            mainViewModel,
-            settingsViewModel,
-            openApp = {
-                openApp(it)
+        AppDetailModal(mainViewModel)
+    } else {
+        AlertDialog(
+            title = {
+                Text(text = stringResource(id = R.string.missing_permission))
             },
-            toAppDetail = {
-                toAppDetail(it)
-            })
+            text = {
+                Text(text = stringResource(id = R.string.request_permission))
+            },
+            onDismissRequest = {
+                mainViewModel.loadApps(context)
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        ActivityHelper.toDetail(context, context.packageName)
+                    }
+                ) {
+                    Text(stringResource(id = R.string.confirm))
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = {
+                        mainViewModel.loadApps(context)
+                    }
+                ) {
+                    Text(stringResource(id = R.string.close))
+                }
+            }
+        )
     }
-    AppDetailModal(mainViewModel)
 }
