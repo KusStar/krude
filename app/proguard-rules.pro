cc91b8ae3a9b5c170f1c58592d346de3b780d11c
Kusstar <kussssss@outlook.com>
2024-05-20 14:05:12 +0800

    wip: hummer js core

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -41,3 +41,5 @@
 # Retain generic signatures of TypeToken and its subclasses with R8 version 3.0 and higher.
 -keep,allowobfuscation,allowshrinking class com.google.gson.reflect.TypeToken
 -keep,allowobfuscation,allowshrinking class * extends com.google.gson.reflect.TypeToken
+# hummer core js engine
+-keep class com.didi.hummer.core.engine.** {*;}
--- app/src/main/java/com/kuss/krude/MainActivity.kt
+++ app/src/main/java/com/kuss/krude/MainActivity.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.didi.hummer.core.engine.JSValue
+import com.didi.hummer.core.engine.napi.NAPIContext
+import com.didi.hummer.core.engine.napi.jni.JSException
 import com.kuss.krude.ui.AppEntry
 import com.kuss.krude.ui.theme.AppTheme
 import com.kuss.krude.utils.PinyinHelper
+import com.kuss.krude.utils.ToastUtils
 import com.kuss.krude.utils.Umami
 import timber.log.Timber
 
@@ -28,6 +32,35 @@ class MainActivity : ComponentActivity() {
                 AppEntry()
             }
         }
+
+        try {
+            System.loadLibrary("hummer-napi")
+            val jsContext = NAPIContext.create()
+            Timber.d("jsContext loaded")
+
+            JSException.addJSContextExceptionCallback(jsContext) { e ->
+                Timber.e("JS ERROR")
+                Timber.e(e)
+            }
+
+            val script = "var v = {" +
+                    "   a: 11," +
+                    "   b: 111111111111111," +
+                    "   c: 12.34," +
+                    "   d: true," +
+                    "   e: 'Hello Hummer'," +
+                    "   f: { aa: 11}," +
+                    "   g: () => {},;" +
+                    "}; v;"
+            jsContext.evaluateJavaScript(script)
+            val v: JSValue = jsContext.getJSValue("v")
+            Timber.d("jsContext evaluated, v.a = ${v.getInt("a")}")
+            ToastUtils.show(this, v.getInt("a").toString())
+        } catch (e: Exception) {
+            Timber.e("jsContext failed")
+            e.printStackTrace()
+        }
+
     }
 
     companion object {
