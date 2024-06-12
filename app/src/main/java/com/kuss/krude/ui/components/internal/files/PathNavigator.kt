8c5d59d0542c73658fdc764fd8bdaaa8c52873c8
Kusstar <kussssss@outlook.com>
2024-06-12 15:07:17 +0800

    feat: FileExtensions -> add PathNavigator, FileIcon

---
+++ app/src/main/java/com/kuss/krude/ui/components/internal/files/PathNavigator.kt
@@ -0,0 +1,56 @@
+package com.kuss.krude.ui.components.internal.files
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import java.util.Stack
+
+
+class PathNavigator(initialPage: String) {
+    private val backStack = Stack<String>()
+    private val forwardStack = Stack<String>()
+    var currentPath by mutableStateOf("")
+    private var canGoBack by mutableStateOf(false)
+    var canForward by mutableStateOf(false)
+
+    init {
+        goTo(initialPage)
+    }
+
+    private fun updateNavigationStates() {
+        canForward = forwardStack.isNotEmpty()
+        canGoBack = backStack.isNotEmpty()
+    }
+
+    fun goTo(page: String) {
+        backStack.push(currentPath)
+        currentPath = page
+        forwardStack.clear()
+        updateNavigationStates()
+    }
+
+    fun goBack(): String {
+        if (!backStack.isEmpty()) {
+            forwardStack.push(currentPath)
+            currentPath = backStack.pop()
+            updateNavigationStates()
+        }
+        return currentPath
+    }
+
+    fun goForward(): String {
+        if (!forwardStack.isEmpty()) {
+            backStack.push(currentPath)
+            currentPath = forwardStack.pop()
+            updateNavigationStates()
+        }
+        return currentPath
+    }
+}
+
+@Composable
+fun rememberPathNavigator(initialPage: String): PathNavigator {
+    return remember { PathNavigator(initialPage) }
+}
