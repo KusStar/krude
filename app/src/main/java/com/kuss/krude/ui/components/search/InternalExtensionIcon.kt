4272c04413dada0ce963cb4059a35394b8c497b7
kusstar <kussssss@outlook.com>
2024-06-16 17:58:45 +0800

    feat(StarListModal): show InternalExtensionIcon

--- app/src/main/java/com/kuss/krude/ui/components/search/InternalExtensionIcon.kt
+++ app/src/main/java/com/kuss/krude/ui/components/search/InternalExtensionIcon.kt
@@ -7,18 +7,19 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
 import com.kuss.krude.extensions.InternalExtensions
 import com.kuss.krude.interfaces.Extension
 import com.kuss.krude.utils.SizeConst
 
 @Composable
-fun InternalExtensionIcon(extension: Extension) {
+fun InternalExtensionIcon(extension: Extension, size: Dp = SizeConst.SEARCH_RESULT_SMALL_ICON_SIZE) {
     if (extension.id == InternalExtensions.FILES_EXTENSION_ID) {
         Icon(
             Icons.Default.Folder,
             tint = MaterialTheme.colorScheme.primary,
             contentDescription = "Files",
-            modifier = Modifier.size(SizeConst.SEARCH_RESULT_SMALL_ICON_SIZE)
+            modifier = Modifier.size(size)
         )
     }
 }
